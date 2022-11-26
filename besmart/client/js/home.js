bb1868ef7a5c19e41bbba4f3045751cc88511b94
FloreaClaudiu <30389908+FloreaClaudiu21@users.noreply.github.com>
2022-11-26 11:25:12 +0200

    changed equipment image size and added movement to the canvas equipments.

--- besmart/client/js/home.js
+++ besmart/client/js/home.js
@@ -1,131 +1,299 @@
 class Equipment {
-    id;
-    name;
-    numberOfInputs;
-    numberOfOutputs;
-    requiredCurrent;
-    requiredVoltage;
-    description;
-    connections;
-    pathToImage;
-    xCoord;
-    yCoord;
+	id;
+	name;
+	sizeW = 15;
+	sizeH = 15;
+	numberOfInputs;
+	numberOfOutputs;
+	requiredCurrent;
+	requiredVoltage;
+	description;
+	connections;
+	pathToImage;
+	xCoord;
+	yCoord;
+	Image;
+	Box;
+	clicked = false;
 
-    constructor(name, numberOfInputs, numberOfOutputs, requiredCurrent = 0, requiredVoltage = 0, description = '', pathToImage = '') {
-        this.name = name;
-        this.description = description;
-        this.numberOfInputs = numberOfInputs;
-        this.numberOfOutputs = numberOfOutputs;
-        this.requiredCurrent = requiredCurrent;
-        this.requiredVoltage = requiredVoltage;
-        this.pathToImage = pathToImage;
-        this.connections = [];
-    }
+	constructor(
+		name,
+		numberOfInputs,
+		numberOfOutputs,
+		requiredCurrent = 0,
+		requiredVoltage = 0,
+		description = "",
+		pathToImage = ""
+	) {
+		this.name = name;
+		this.description = description;
+		this.numberOfInputs = numberOfInputs;
+		this.numberOfOutputs = numberOfOutputs;
+		this.requiredCurrent = requiredCurrent;
+		this.requiredVoltage = requiredVoltage;
+		this.pathToImage = pathToImage;
+		if (this.pathToImage != null && this.pathToImage.length > 0) {
+			this.Image = new Image(this.sizeW, this.sizeH);
+			this.Image.src = this.pathToImage;
+		}
+		this.connections = [];
+		return;
+	}
 
-    connectWith(equipment) {
-        this.connections.push(equipment);
-    }
+	connectWith(equipment) {
+		this.connections.push(equipment);
+	}
+
+	isMouseInShape = (x, y) => {
+		let shape_left = this.xCoord;
+		let shape_right = this.xCoord + this.sizeW;
+		let shape_top = this.yCoord;
+		let shape_bottom = this.yCoord + this.sizeH;
+		if (
+			x >= shape_left &&
+			x <= shape_right &&
+			y >= shape_top &&
+			y <= shape_bottom
+		) {
+			return true;
+		}
+		return false;
+	};
+
+	draw(ctx) {
+		ctx.lineWidth = 1;
+		ctx.setLineDash([2]);
+		ctx.strokeStyle = "gray";
+		this.clicked &&
+			ctx.strokeRect(
+				this.xCoord - 1,
+				this.yCoord - 1,
+				this.sizeW + 2,
+				this.sizeH + 3
+			);
+		ctx.drawImage(this.Image, this.xCoord, this.yCoord, this.sizeW, this.sizeH);
+		return;
+	}
 }
-
 class Simulation {
-    constructor() {
-        this.equipments = [];
-    }
+	constructor() {
+		this.equipments = [];
+	}
 
-    addEquipment(equipment) {
-        this.equipments.push(equipment);
-    }
+	existsEquipment(equipment) {
+		for (const eq of this.equipments) {
+			if (eq.name === equipment.name) return true;
+		}
+		return false;
+	}
 
-    removeEquipment(equipment) {
-        const equipmentIndex = this.equipments.indexOf(equipment);
-        this.equipments.splice(equipmentIndex, 1);    
-    }
+	addEquipment(equipment) {
+		if (this.existsEquipment(equipment)) {
+			alert("Equipment already exists on the canvas!");
+			return false;
+		}
+		this.equipments.push(equipment);
+		return true;
+	}
 
-    test() {
-        // TODO: implement test later
-    }
+	removeEquipment(equipment) {
+		const equipmentIndex = this.equipments.indexOf(equipment);
+		this.equipments.splice(equipmentIndex, 1);
+	}
 
-    connect(equipment1, equipment2) {
-        if (this.checkIfConnectionPossible(equipment1, equipment2)) {
-            const indexOfEquipment1 = this.equipments.indexOf(equipment1);
-            this.equipments[indexOfEquipment1].connectWith(equipment2);
+	connect(equipment1, equipment2) {
+		if (this.checkIfConnectionPossible(equipment1, equipment2)) {
+			const indexOfEquipment1 = this.equipments.indexOf(equipment1);
+			this.equipments[indexOfEquipment1].connectWith(equipment2);
 
-            const indexOfEquipment2 = this.equipments.indexOf(equipment2);
-            this.equipments[indexOfEquipment2].connectWith(equipment1);
-        } else {
-            throw "Unable to connect equipments";
-        }
-    }
+			const indexOfEquipment2 = this.equipments.indexOf(equipment2);
+			this.equipments[indexOfEquipment2].connectWith(equipment1);
+		} else {
+			throw "Unable to connect equipments";
+		}
+	}
 
-    checkIfConnectionPossible(equipment1, equipment2) {
-        if (equipment1.numberOfOutputs > equipment2.numberOfInputs) {
-            return false;
-        }
-        return true;
-    }
+	checkIfConnectionPossible(equipment1, equipment2) {
+		if (equipment1.numberOfOutputs > equipment2.numberOfInputs) {
+			return false;
+		}
+		return true;
+	}
 }
-
+class Canvas {
+	canvas;
+	selectedEquipment = null;
+	constructor(simulation) {
+		this.sim = simulation;
+		this.canvas = document.getElementById("viewport");
+		this.ctx = this.canvas.getContext("2d");
+		this.addEvents();
+		return;
+	}
+	getCanvasBox = () => {
+		return this.canvas.getBoundingClientRect();
+	};
+	getMousePos = (evt) => {
+		let rect = this.getCanvasBox();
+		return {
+			x:
+				((evt.clientX - rect.left) / (rect.right - rect.left)) *
+				this.canvas.width,
+			y:
+				((evt.clientY - rect.top) / (rect.bottom - rect.top)) *
+				this.canvas.height,
+		};
+	};
+	spawn = (equipment) => {
+		if (!equipment.xCoord) {
+			equipment.xCoord = 20;
+		}
+		if (!equipment.yCoord) {
+			equipment.yCoord = 50;
+		}
+		this.draw();
+		return;
+	};
+	equipmentAtLocation = (e) => {
+		let startX = this.getMousePos(e).x;
+		let startY = this.getMousePos(e).y;
+		for (let equipment of this.sim.equipments) {
+			if (equipment.isMouseInShape(startX, startY)) {
+				return equipment;
+			}
+		}
+		return null;
+	};
+	draw = () => {
+		let bounding = this.getCanvasBox();
+		this.ctx.clearRect(0, 0, bounding.width, bounding.height);
+		for (const equipment of this.sim.equipments) {
+			equipment.draw(this.ctx);
+		}
+		return;
+	};
+	addEvents = () => {
+		this.canvas.addEventListener("mouseleave", () => {
+			if (this.selectedEquipment == null) return;
+			this.selectedEquipment.clicked = false;
+			this.selectedEquipment = null;
+			this.draw();
+			return;
+		});
+		this.canvas.addEventListener("mousedown", (e) => {
+			const eq = this.equipmentAtLocation(e);
+			///////////////////////////////////////
+			if (eq == null) return;
+			eq.clicked = true;
+			this.selectedEquipment = eq;
+			this.draw();
+			return;
+		});
+		this.canvas.addEventListener("mousemove", (e) => {
+			if (this.selectedEquipment == null) return;
+			let maxX = 285,
+				maxY = 135;
+			const mousePos = this.getMousePos(e);
+			let newX = mousePos.x;
+			let newY = mousePos.y;
+			//////////////////////
+			if (newX >= maxX) {
+				newX = maxX;
+			}
+			if (newY >= maxY) {
+				newY = maxY;
+			}
+			this.selectedEquipment.xCoord = newX;
+			this.selectedEquipment.yCoord = newY;
+			this.draw();
+			return;
+		});
+		this.canvas.addEventListener("mouseup", (e) => {
+			if (this.selectedEquipment == null) return;
+			this.selectedEquipment.clicked = false;
+			this.selectedEquipment = null;
+			this.draw();
+			return;
+		});
+	};
+}
 
 const EQUIPMENTS = [
-    new Equipment("LED", 2, 1, 0, 0, '', 'D:\\LUCHICI\\Web Apps Programming 1\\Source\\rau-web-apps-programming-1-g610-2022-2023\\besmart\\client\\assets\\simulation\\led.png'),
-    new Equipment("Battery", 0, 2, 0, 0, '', 'D:\\LUCHICI\\Web Apps Programming 1\\Source\\rau-web-apps-programming-1-g610-2022-2023\\besmart\\client\\assets\\simulation\\battery.png'),
-    new Equipment("Resistor", 1, 1, 0, 0, '', 'D:\\LUCHICI\\Web Apps Programming 1\\Source\\rau-web-apps-programming-1-g610-2022-2023\\besmart\\client\\assets\\simulation\\resistor.png'),
-    new Equipment("Capacitor", 1, 1, 0, 0, '', 'D:\\LUCHICI\\Web Apps Programming 1\\Source\\rau-web-apps-programming-1-g610-2022-2023\\besmart\\client\\assets\\simulation\\capacitor.png')
-]
-
+	new Equipment("LED", 2, 1, 0, 0, "", "../client/assets/simulation/led.png"),
+	new Equipment(
+		"Battery",
+		0,
+		2,
+		0,
+		0,
+		"",
+		"../client/assets/simulation/battery.png"
+	),
+	new Equipment(
+		"Resistor",
+		1,
+		1,
+		0,
+		0,
+		"",
+		"../client/assets/simulation/resistor.png"
+	),
+	new Equipment(
+		"Capacitor",
+		1,
+		1,
+		0,
+		0,
+		"",
+		"../client/assets/simulation/capacitor.png"
+	),
+];
 const simulation = new Simulation();
-// simulation.addEquipment(EQUIPMENTS[0]);
-// simulation.addEquipment(EQUIPMENTS[1]);
-// simulation.connect(simulation.equipments[1], simulation.equipments[0]);
-// console.log(simulation);
-
+const canvas = new Canvas(simulation);
 
 function addEquipmentsToDropdown(equipments) {
-    const equipmentsDropdown = document.getElementById("equipment-select");
-    if (equipmentsDropdown) {
-        const option = document.createElement("option");
-        option.value = "Select equipment...";
-        option.innerText ="Select equipment...";
-        equipmentsDropdown.appendChild(option);
-        for (const equipment of equipments) {
-            const option = document.createElement("option");
-            option.value = ""
-            option.innerText = equipment.name;
-            equipmentsDropdown.appendChild(option);
-        }
-    }
+	const equipmentsDropdown = document.getElementById("equipment-select");
+	if (equipmentsDropdown) {
+		const option = document.createElement("option");
+		option.value = "Select equipment...";
+		option.innerText = "Select equipment...";
+		equipmentsDropdown.appendChild(option);
+		for (const equipment of equipments) {
+			const option = document.createElement("option");
+			option.value = "";
+			option.innerText = equipment.name;
+			equipmentsDropdown.appendChild(option);
+		}
+	}
 }
 
-
 function getSelectedEquipment() {
-    const select = document.getElementById("equipment-select");
-    const equipmentName = select.options[select.selectedIndex].text;
-    for (const equipment of EQUIPMENTS) {
-        if (equipment.name === equipmentName) {
-            simulation.addEquipment(equipment);
-            regenerateSimulationCanvas(simulation);
-            break;
-        }
-    }
-    select.selectedIndex = 0;
+	const select = document.getElementById("equipment-select");
+	const equipmentName = select.options[select.selectedIndex].text;
+	for (const equipment of EQUIPMENTS) {
+		if (equipment.name === equipmentName) {
+			simulation.addEquipment(equipment) && canvas.spawn(equipment);
+			break;
+		}
+	}
+	select.selectedIndex = 0;
+	return;
 }
 
 function createEquipmentsDropdown(equipments) {
-    const select = document.createElement("select");
-    select.name = "equipment";
-    select.id = "equipment-select";
-    select.onchange = getSelectedEquipment;
-    const addEquipmentButton = document.getElementById("add-equipment-button");
-    addEquipmentButton.appendChild(select);
-
-    addEquipmentsToDropdown(equipments);
+	const select = document.createElement("select");
+	select.name = "equipment";
+	select.id = "equipment-select";
+	select.onchange = getSelectedEquipment;
+	const addEquipmentButton = document.getElementById("add-equipment-button");
+	addEquipmentButton.appendChild(select);
+	addEquipmentsToDropdown(equipments);
+	return;
 }
 
 createEquipmentsDropdown(EQUIPMENTS);
 
-// const img = new Image();
-
-function regenerateSimulationCanvas(simulation) {
+/*function regenerateSimulationCanvas(simulation) {
     // TODO: make equipments 15px by 15px on canvas
     const canvas = document.getElementById("viewport");
     const ctx = canvas.getContext("2d");
@@ -157,6 +325,7 @@ function regenerateSimulationCanvas(simulation) {
         ctx.drawImage(img, x, y);
     }
 }
+*/
 
 // TODO: Add method to move one equipment once added to canvas
 // https://stackoverflow.com/questions/55633974/how-do-you-move-objects-by-dragging-on-the-background-in-html5-canvas
